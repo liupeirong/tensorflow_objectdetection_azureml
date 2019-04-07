2843eff7250e0c5017cac8614eb71efc913ad8e5
paige.liu <pliu@microsoft.com>
2019-04-07 20:10:24 +0000

    Deploy to Azure ML

---
+++ aml_deploy/deploy_to_aks.py
@@ -0,0 +1,127 @@
+import string
+import os
+import argparse
+import shutil
+
+import azureml.core
+from azureml.core import Workspace, Datastore
+from azureml.core.model import Model
+from azureml.core.webservice import Webservice, AksWebservice
+from azureml.core.image import ContainerImage, Image
+
+print("SDK version:", azureml.core.VERSION)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--proj_root', 
+                    help='root directory in Azure ML data store that contains training data and base model',
+                    required=True)
+parser.add_argument('--model_version', type=int,
+                    help='the version of the model to deploy',
+                    required=True)
+parser.add_argument('--support_gpu', type=bool, 
+                    help='Whether to inference with gpu-enabled compute',
+                    default=False)
+FLAGS = parser.parse_args()
+
+# input parameters
+proj_root=FLAGS.proj_root
+model_version = FLAGS.model_version
+support_gpu = FLAGS.support_gpu
+
+# environment variables
+subscription_id = os.getenv('SUBSCRIPTION_ID')
+resource_group = os.getenv('RESOURCE_GROUP')
+workspace_name = os.getenv('WORKSPACE_NAME')
+proj_datastore = os.getenv('PROJ_DATASTORE', default=None)
+compute_cpu = os.getenv('AML_AKS_CPU', default='akscpu')
+compute_gpu = os.getenv('AML_AKS_GPU', default='aksnv6')
+
+# constants
+DATA_SUBDIR='data'
+TFRECORDS_SUBDIR='tfrecords'
+MODELS_SUBDIR='models'
+PASCAL_LABEL_MAP_FILE='pascal_label_map.pbtxt'
+
+# set up Azure ML environment
+ws = Workspace(subscription_id = subscription_id, resource_group = resource_group, workspace_name = workspace_name)
+if proj_datastore is None:
+    ds = ws.get_default_datastore()
+else:
+    ds = Datastore.get(ws, datastore_name=proj_datastore)
+compute_name = compute_gpu if support_gpu else compute_cpu
+
+#   derive model_name from proj_root, same logic as in training
+model_name = proj_root if proj_root.isalnum() else ''.join(ch for ch in proj_root if ch.isalnum())
+model = None
+models = Model.list(ws, name=model_name)
+for m in models:
+    if m.version == model_version:
+        model = m
+        break
+if model is None:
+    raise ValueError('model {}:{} not found'.format(model_name, model_version))
+print("proj_root:{}, model:{}:{}".format(proj_root, model_name, model_version))
+
+pascal_label_map_in_ds = os.path.join(proj_root, TFRECORDS_SUBDIR, PASCAL_LABEL_MAP_FILE)
+inference_docker_image = model_name
+webservice_name = model_name + 'svc'
+
+# Build the inference image
+#   inference depends on 2 files:
+#       1) the frozen model 
+#       2) the label map so we can display human-readble labels 
+ds.download(target_path='.',
+            prefix=pascal_label_map_in_ds,
+            overwrite=True,
+            show_progress=True) 
+shutil.copy(pascal_label_map_in_ds, '.') #copy to current folder
+
+#   score.py has to load the model, but there's no way to pass in a parameter or set env variable to the image or web service
+with open("score.py", "rt") as fin:
+    with open("mscore.py", "wt") as fout:
+        for line in fin:
+            fout.write(line.replace('__REPLACE_MODEL_NAME__', model_name))
+
+image_config = ContainerImage.image_configuration(
+    execution_script = "mscore.py",
+    runtime = "python",
+    conda_file = "conda_env.yml",
+    description = model_name,
+    dependencies = ['./', 'utils/'],
+    enable_gpu = support_gpu)
+
+image = ContainerImage.create(name = inference_docker_image, 
+                              models = [model], 
+                              image_config = image_config,
+                              workspace = ws
+                              )
+image.wait_for_creation(show_output=True)
+inference_docker_image_version = image.version
+print('created image: {}:{}'.format(inference_docker_image, inference_docker_image_version))
+
+# Deploy a new web service or update an existing web service with the image
+def deploy_new_webservice(ws, compute_name, webservice_name, image):
+    aks_target = ws.compute_targets[compute_name]
+    aks_config = AksWebservice.deploy_configuration(collect_model_data=True, enable_app_insights=True)
+    service = Webservice.deploy_from_image(
+        workspace = ws, 
+        name = webservice_name,
+        image = image,
+        deployment_config = aks_config,
+        deployment_target = aks_target)
+    service.wait_for_deployment(show_output = True)
+    print(service.state)
+
+def update_existing_webservice(service, image):
+    service.update(image = image)
+    service.wait_for_deployment(show_output = True)
+    print(service.state)
+
+# Check if the target service already exists
+try: 
+    service = Webservice(name = webservice_name, workspace = ws)
+except:
+    deploy_new_webservice(ws, compute_name, webservice_name, image)
+else:
+    update_existing_webservice(service, image)
+
---
+++ aml_deploy/score.py
@@ -0,0 +1,136 @@
+import base64
+import json
+import os
+import time
+import yaml
+from io import BytesIO
+import logging
+import logging.config
+
+import numpy as np
+import tensorflow as tf
+from azureml.core.model import Model
+from PIL import Image
+
+from utils import label_map_util
+from azureml.monitoring import ModelDataCollector
+
+MODEL_NAME = '__REPLACE_MODEL_NAME__'
+PASCAL_LABEL_MAP_FILE = 'pascal_label_map.pbtxt'
+THRESHOLD = float(os.getenv('MIN_CONFIDENCE', '0.8'))
+
+def init():
+  global logger
+  global model
+  global data_collector
+  init_logger()
+  data_collector = ModelDataCollector(MODEL_NAME, identifier="imgpred", feature_names=["imageb64", "prediction"])
+  model = load_model()
+
+def init_logger():
+  global logger
+  logconf = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logging.yml')
+  print('logconf {}'.format(logconf))
+  if os.path.exists(logconf):
+    with open(logconf, 'rt') as f:
+      config = yaml.safe_load(f.read())
+      logging.config.dictConfig(config)
+      print('logconf loaded')
+  else:
+      logging.basicConfig(level=default_level)
+      print('logconf fall back to default')
+  logger = logging.getLogger('score')
+
+
+def load_model():
+  # figure out how many classes
+  label_map_dict = label_map_util.get_label_map_dict(PASCAL_LABEL_MAP_FILE)
+  num_classes = len(label_map_dict)
+  logger.info('num_of_classes in pascal_label_map: {}'.format(num_classes))
+
+  # Load label map
+  label_map = label_map_util.load_labelmap(PASCAL_LABEL_MAP_FILE)
+  categories = label_map_util.convert_label_map_to_categories(
+    label_map, max_num_classes=num_classes, use_display_name=True)
+  category_index = label_map_util.create_category_index(categories)
+
+  # Load a frozen Tensorflow model into memory
+  model_path = Model.get_model_path(MODEL_NAME)
+  logger.info('getting model {} from {}'.format(MODEL_NAME, model_path))
+
+  detection_graph = tf.Graph()
+  with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(model_path, 'rb') as fid:
+      serialized_graph = fid.read()
+      od_graph_def.ParseFromString(serialized_graph)
+      tf.import_graph_def(od_graph_def, name='')
+
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs }
+      tensor_dict = {}
+      for key in ['num_detections', 'detection_boxes', 'detection_scores', 'detection_classes']:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(tensor_name)
+
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+      sess = tf.Session(graph=detection_graph)
+  return {
+    'session': sess,
+    'image_tensor': image_tensor,
+    'tensor_dict': tensor_dict,
+    'category_index': category_index
+  }
+
+
+def run(raw_data):
+  logger.debug('raw_data: {}'.format(raw_data))
+  try:
+    response = inference(raw_data)
+  except Exception as e:
+    response = str(e)
+  return response
+
+
+def inference(raw_data):
+  logger.info('parse input json raw_data to get image')
+  parsed_json = json.loads(raw_data)
+  image_raw = parsed_json['file']
+  logger.info('base64 decode input image')
+  image = Image.open(BytesIO(base64.b64decode(image_raw)))
+  logger.info('turn decoded image to np_array')
+  (im_width, im_height) = image.size
+  image_np = np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)
+  image_np_expanded = np.expand_dims(image_np, axis=0)
+
+  # Run inference
+  start_time = time.time()
+  output_dict = model['session'].run(
+    model['tensor_dict'], feed_dict={model['image_tensor']: image_np_expanded})
+  latency = time.time() - start_time
+  logger.info('scoring took {} seconds'.format(latency))
+
+  # all outputs are float32 numpy arrays, so convert types as appropriate
+  output_dict['num_detections'] = int(output_dict['num_detections'][0])
+  output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(np.uint8).tolist()
+  output_dict['detection_boxes'] = output_dict['detection_boxes'][0].tolist()
+  output_dict['detection_scores'] = output_dict['detection_scores'][0].tolist()
+  logger.info('num of detections: {}'.format(output_dict['num_detections']))
+
+  result = []
+  for idx, score in enumerate(output_dict['detection_scores']):
+    if score > THRESHOLD:
+      result.append({
+        'class': output_dict['detection_classes'][idx],
+        'label': model['category_index'][output_dict['detection_classes'][idx]]['name'],
+        'confidence': output_dict['detection_scores'][idx],
+        'bounding_box': output_dict['detection_boxes'][idx]
+      })
+    else:
+      logger.debug('idx {} detection score too low {}'.format(idx, score))
+  #inputs_dc.collect([image_raw]) #can only be 2-d or list
+  data_collector.collect([1], result)
+  return result
+
